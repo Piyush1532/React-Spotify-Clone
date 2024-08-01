4f210445a288ad09da4fe90269e8f70350297b60
Piyush1532 <piyushadav503@gmail.com>
2024-08-01 16:01:05 +0530

    updated

--- src/Components/Displayhome.jsx
+++ src/Components/Displayhome.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import Navbar from './Navbar'
+import { albumsData } from '../assets/assets'
+import Albumitem from './Albumitem'
 
 const Displayhome = () => {
   return (
     <>
       <Navbar/>
-      <div className="my-5 font-bold text-2xl">
-  
+      <div className="mb-4">
+        <h1 className='my-5 font-bold text-2xl'>Fearured Charts</h1>
+        <div className='flex overflow-auto'>
+        {albumsData.map((item,index)=>{
+<Albumitem key={index} name={item.name} desc={item.desc} id={item.id} image={item.image}/>
+  })}
+        </div>
+
       </div>
     </>
   )

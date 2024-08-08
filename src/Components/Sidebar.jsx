fffe4e35e4e37b68ff82352765d777d533e78a34
Piyush1532 <piyushadav503@gmail.com>
2024-08-08 14:22:06 +0530

    updated some basic design

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -4,7 +4,7 @@ const Sidebar = () => {
   return (
     <div className='w-[25%] h-full p-2 flex-col gap-2 text-white hidden lg:flex'>
       <div className='bg-[#121212] h=[15%] rounded flex flex-col justify-around'>
-        <div className=' flex items-center gap-3 pl-8 cursor-pointer'>
+        <div className=' flex items-center gap-3 pl-8 cursor-pointer mb-3'>
     <img className='w-6' src={assets.home_icon} alt="" />
     <p className='font-bold'>Home</p>
         </div>

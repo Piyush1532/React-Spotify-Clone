072682a4988d716dbb4e34ecd96a7fe9f40f7763
Piyush1532 <piyushadav503@gmail.com>
2024-08-08 14:19:52 +0530

    added album items

--- src/Components/Albumitem.jsx
+++ src/Components/Albumitem.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Albumitem = ({image,name,desc,id}) => {
   return (
-    <div className='min-w-[180px] p-2 px3 rounded cursor-pointer hover:bg-[ffffff26]'>
+    <div className='min-w-[180px] p-2 px3 rounded cursor-pointer hover:bg-[#ffffff26]'>
       <img className='rounded ' src={image} alt="" />
       <p className='font-bold mt-2 mb-2'>{name}</p>
       <p className='text-slate-200 text-sm'>{desc}</p>

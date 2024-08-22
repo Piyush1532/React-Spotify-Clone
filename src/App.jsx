204388e7bb46537c05449fafd2804f1e8ad150bf
Piyush1532 <piyushadav503@gmail.com>
2024-08-22 15:19:46 +0530

    updated with context provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Sidebar from './Components/Sidebar'
 import Player from './Components/Player'
 import Display from './Components/Display'
+import { PlayerContext } from './Context/playerContext'
 
 const App = () => {
+
+const {audioRef,track}=useContext(PlayerContext)
+
   return (
     <div className='h-screen bg-black'>
     <div className=" h-[90%] flex">
@@ -12,6 +16,7 @@ const App = () => {
       
     </div>
       <Player/>
+      <audio ref={audioRef} src={track.file} preload='auto'></audio>
     </div>
   )
 }
